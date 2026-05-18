1caf02b988db4111cc21ecb106898cdc51c7234f
Elie Cohen <ec92009@gmail.com>
2026-05-18 13:00:08 +0200

    photosbyelie: sync private delivery manifests

--- home-data.js
+++ home-data.js
@@ -4,9 +4,70 @@ window.photosByElieHomeData = {
     "title": "France",
     "description": "Saturn Lightroom archive selections prepared from the Camera source.",
     "accent": "france-gallery",
-    "count": 0,
+    "count": 28,
     "href": "./gallery.html?gallery=france",
-    "photos": []
+    "photos": [
+      {
+        "id": "20220506-160631-03403-51426edaac",
+        "title": "Les Invalides, Paris",
+        "gallerySrc": "",
+        "imageSrc": "",
+        "media": {
+          "type": "photo",
+          "sourcePolicy": "developed-master",
+          "publicPreview": {
+            "allowed": true,
+            "galleryKey": "expo/20220506-160631-03403-51426edaac_900.jpg",
+            "detailKey": "expo/20220506-160631-03403-51426edaac_1800.jpg"
+          }
+        }
+      },
+      {
+        "id": "20220506-154558-03388-a887904b4b",
+        "title": "Eglise des Invalides, Paris",
+        "gallerySrc": "",
+        "imageSrc": "",
+        "media": {
+          "type": "photo",
+          "sourcePolicy": "developed-master",
+          "publicPreview": {
+            "allowed": true,
+            "galleryKey": "expo/20220506-154558-03388-a887904b4b_900.jpg",
+            "detailKey": "expo/20220506-154558-03388-a887904b4b_1800.jpg"
+          }
+        }
+      },
+      {
+        "id": "20220506-151024-03375-3e4d581417",
+        "title": "Les Invalides",
+        "gallerySrc": "",
+        "imageSrc": "",
+        "media": {
+          "type": "photo",
+          "sourcePolicy": "developed-master",
+          "publicPreview": {
+            "allowed": true,
+            "galleryKey": "expo/20220506-151024-03375-3e4d581417_900.jpg",
+            "detailKey": "expo/20220506-151024-03375-3e4d581417_1800.jpg"
+          }
+        }
+      },
+      {
+        "id": "20220506-145942-03357-6183b42671",
+        "title": "20220506 145942 03357",
+        "gallerySrc": "",
+        "imageSrc": "",
+        "media": {
+          "type": "photo",
+          "sourcePolicy": "developed-master",
+          "publicPreview": {
+            "allowed": true,
+            "galleryKey": "expo/20220506-145942-03357-6183b42671_900.jpg",
+            "detailKey": "expo/20220506-145942-03357-6183b42671_1800.jpg"
+          }
+        }
+      }
+    ]
   },
   "usa": {
     "number": "02",
@@ -22,7 +83,7 @@ window.photosByElieHomeData = {
     "title": "Spain",
     "description": "Saturn Lightroom archive selections prepared from the Camera source.",
     "accent": "spain-gallery",
-    "count": 14,
+    "count": 30,
     "href": "./gallery.html?gallery=spain",
     "photos": [
       {
@@ -101,9 +162,84 @@ window.photosByElieHomeData = {
     "title": "AI",
     "description": "Leonardo archive selections prepared from the Saturn Lightroom AI source.",
     "accent": "ai-gallery",
-    "count": 0,
+    "count": 71,
     "href": "./gallery.html?gallery=ai",
-    "photos": []
+    "photos": [
+      {
+        "id": "cabin-e09804f1d1",
+        "title": "Cabin",
+        "gallerySrc": "",
+        "imageSrc": "",
+        "media": {
+          "type": "video",
+          "sourcePolicy": "developed-master",
+          "publicPreview": {
+            "allowed": true,
+            "galleryKey": "expo/cabin-e09804f1d1_900.jpg",
+            "detailKey": "expo/cabin-e09804f1d1_short_5s_720p.mp4",
+            "dimensions": {
+              "width": 1280,
+              "height": 720
+            }
+          },
+          "video": {
+            "duration": 23.76
+          }
+        }
+      },
+      {
+        "id": "beyrouth-630240f1ea",
+        "title": "Beyrouth",
+        "gallerySrc": "",
+        "imageSrc": "",
+        "media": {
+          "type": "video",
+          "sourcePolicy": "developed-master",
+          "publicPreview": {
+            "allowed": true,
+            "galleryKey": "expo/beyrouth-630240f1ea_900.jpg",
+            "detailKey": "expo/beyrouth-630240f1ea_short_5s_720p.mp4",
+            "dimensions": {
+              "width": 1280,
+              "height": 720
+            }
+          },
+          "video": {
+            "duration": 43.0
+          }
+        }
+      },
+      {
+        "id": "wellness-retreats-yoga-meditation-holistic-treatments-h-3-upscale-19f8818a-cf96-4121-9812-2baa1f7175d4-b731a67d69",
+        "title": "Wellness Retreats - Yoga, meditation, holistic treatments, healing springs, organic cuisine, detoxing, spiritual tourism",
+        "gallerySrc": "",
+        "imageSrc": "",
+        "media": {
+          "type": "photo",
+          "sourcePolicy": "developed-master",
+          "publicPreview": {
+            "allowed": true,
+            "galleryKey": "expo/wellness-retreats-yoga-meditation-holistic-treatments-h-3-upscale-19f8818a-cf96-4121-9812-2baa1f7175d4-b731a67d69_900.jpg",
+            "detailKey": "expo/wellness-retreats-yoga-meditation-holistic-treatments-h-3-upscale-19f8818a-cf96-4121-9812-2baa1f7175d4-b731a67d69_1800.jpg"
+          }
+        }
+      },
+      {
+        "id": "wellness-retreats-yoga-meditation-holistic-treatments-h-2-upscale-e3b7f2b7-c7a5-4c49-a9ca-c5d0d004c373-e56426a7e1",
+        "title": "Wellness Retreats - Yoga, meditation, holistic treatments, healing springs, organic cuisine, detoxing, spiritual tourism",
+        "gallerySrc": "",
+        "imageSrc": "",
+        "media": {
+          "type": "photo",
+          "sourcePolicy": "developed-master",
+          "publicPreview": {
+            "allowed": true,
+            "galleryKey": "expo/wellness-retreats-yoga-meditation-holistic-treatments-h-2-upscale-e3b7f2b7-c7a5-4c49-a9ca-c5d0d004c373-e56426a7e1_900.jpg",
+            "detailKey": "expo/wellness-retreats-yoga-meditation-holistic-treatments-h-2-upscale-e3b7f2b7-c7a5-4c49-a9ca-c5d0d004c373-e56426a7e1_1800.jpg"
+          }
+        }
+      }
+    ]
   },
   "italy": {
     "number": "06",
@@ -119,7 +255,7 @@ window.photosByElieHomeData = {
     "title": "Portugal",
     "description": "Saturn Lightroom archive selections prepared from the Camera source.",
     "accent": "portugal-gallery",
-    "count": 1,
+    "count": 20,
     "href": "./gallery.html?gallery=portugal",
     "photos": [
       {
@@ -134,6 +270,51 @@ window.photosByElieHomeData = {
             "allowed": true,
             "galleryKey": "expo/20220802-130003-00136-pano-052608ca90_900.jpg",
             "detailKey": "expo/20220802-130003-00136-pano-052608ca90_1800.jpg"
+          }
+        }
+      },
+      {
+        "id": "20220521-145332-04573-0742e2b77c",
+        "title": "20220521 145332 04573",
+        "gallerySrc": "",
+        "imageSrc": "",
+        "media": {
+          "type": "photo",
+          "sourcePolicy": "developed-master",
+          "publicPreview": {
+            "allowed": true,
+            "galleryKey": "expo/20220521-145332-04573-0742e2b77c_900.jpg",
+            "detailKey": "expo/20220521-145332-04573-0742e2b77c_1800.jpg"
+          }
+        }
+      },
+      {
+        "id": "20220521-145324-04568-1c242c532c",
+        "title": "20220521 145324 04568",
+        "gallerySrc": "",
+        "imageSrc": "",
+        "media": {
+          "type": "photo",
+          "sourcePolicy": "developed-master",
+          "publicPreview": {
+            "allowed": true,
+            "galleryKey": "expo/20220521-145324-04568-1c242c532c_900.jpg",
+            "detailKey": "expo/20220521-145324-04568-1c242c532c_1800.jpg"
+          }
+        }
+      },
+      {
+        "id": "20220521-144639-04546-3394e60cbe",
+        "title": "20220521 144639 04546",
+        "gallerySrc": "",
+        "imageSrc": "",
+        "media": {
+          "type": "photo",
+          "sourcePolicy": "developed-master",
+          "publicPreview": {
+            "allowed": true,
+            "galleryKey": "expo/20220521-144639-04546-3394e60cbe_900.jpg",
+            "detailKey": "expo/20220521-144639-04546-3394e60cbe_1800.jpg"
           }
         }
       }

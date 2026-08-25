46abdb2fdc2edb0982461fe04caa26faa1f8fbd0
Elie Cohen <ec92009@gmail.com>
2026-08-25 20:59:30 +0200

    photosbyelie: prepare Backstage 237.12

--- native/PhotosByElieBackstage/release-metadata.zsh
+++ native/PhotosByElieBackstage/release-metadata.zsh
@@ -3,7 +3,7 @@
 # release identity.
 
 PBE_BACKSTAGE_BUNDLE_IDENTIFIER="com.photosbyelie.backstage"
-PBE_BACKSTAGE_VERSION="237.11"
-PBE_BACKSTAGE_BUILD="198"
+PBE_BACKSTAGE_VERSION="237.12"
+PBE_BACKSTAGE_BUILD="199"
 PBE_BACKSTAGE_UPDATE_MANIFEST_URL="https://download.photos-by-elie.com/backstage/releases/latest.json"
 PBE_BACKSTAGE_RELEASE_SOURCE_REF="refs/heads/release/backstage"

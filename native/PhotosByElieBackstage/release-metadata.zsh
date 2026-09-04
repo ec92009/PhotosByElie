671c87fdb7c8b956f69c26096d282d7be9c3e213
Elie Cohen <ec92009@gmail.com>
2026-09-04 22:09:54 +0200

    photosbyelie: allocate Backstage build 312

--- native/PhotosByElieBackstage/release-metadata.zsh
+++ native/PhotosByElieBackstage/release-metadata.zsh
@@ -3,7 +3,7 @@
 # release identity.
 
 PBE_BACKSTAGE_BUNDLE_IDENTIFIER="com.photosbyelie.backstage"
-PBE_BACKSTAGE_VERSION="246.14"
-PBE_BACKSTAGE_BUILD="311"
+PBE_BACKSTAGE_VERSION="247.0"
+PBE_BACKSTAGE_BUILD="312"
 PBE_BACKSTAGE_UPDATE_MANIFEST_URL="https://download.photos-by-elie.com/backstage/releases/latest.json"
 PBE_BACKSTAGE_RELEASE_SOURCE_REF="refs/heads/release/backstage"
